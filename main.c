3966f6eaa5ff5b4cda4a766bda57815f4a2c78e6
Mateusz Biesaga <mat86bie@gmail.com>
2015-08-05 09:57:09 +0200

    sending data to the in endpoint refs #2424

--- main.c
+++ main.c
@@ -10,50 +10,61 @@
 #include <avr/wdt.h>
 
 #include "usbdrv.h"
+#include "usbdrv/usbdrv.h"
 
 #define F_CPU 16500000L
 #include <util/delay.h>
 
-#define USB_LED_OFF 0
-#define USB_LED_ON  1
-#define USB_DATA_OUT 2
-#define USB_DATA_WRITE 3
-#define USB_DATA_IN 4
+#define VENDOR_RQ_READ_BUFFER 0x01
+
+PROGMEM const char usbHidReportDescriptor[52] = { /* USB report descriptor, size must match usbconfig.h */
+        0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+        0x09, 0x00,                    // USAGE (Undefined)
+        0xa1, 0x01,                    // COLLECTION (Application)
+        0x09, 0x01,                    //   USAGE (Pointer)
+        0xA1, 0x00,                    //   COLLECTION (Physical)
+        0x05, 0x09,                    //     USAGE_PAGE (Button)
+        0x19, 0x01,                    //     USAGE_MINIMUM
+        0x29, 0x03,                    //     USAGE_MAXIMUM
+        0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+        0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+        0x95, 0x03,                    //     REPORT_COUNT (3)
+        0x75, 0x01,                    //     REPORT_SIZE (1)
+        0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+        0x95, 0x01,                    //     REPORT_COUNT (1)
+        0x75, 0x05,                    //     REPORT_SIZE (5)
+        0x81, 0x03,                    //     INPUT (Const,Var,Abs)
+        0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+        0x09, 0x30,                    //     USAGE (X)
+        0x09, 0x31,                    //     USAGE (Y)
+        0x09, 0x38,                    //     USAGE (Wheel)
+        0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+        0x25, 0x7F,                    //     LOGICAL_MAXIMUM (127)
+        0x75, 0x08,                    //     REPORT_SIZE (8)
+        0x95, 0x03,                    //     REPORT_COUNT (3)
+        0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+        0xC0,                          //   END_COLLECTION
+        0xC0,                          // END COLLECTION
+};
 
 static uchar replyBuf[16] = "Hello, USB!";
 static uchar dataReceived = 0, dataLength = 0; // for USB_DATA_IN
 
+static uchar reportBuffer = 0;
+static uchar currentPosition, bytesRemaining, idleRate;
+
 // this gets called when custom control message is received
-USB_PUBLIC uchar usbFunctionSetup(uchar data[8]) {
-	usbRequest_t *rq = (void *)data; // cast data to correct type
-	
-	switch(rq->bRequest) { // custom command is in the bRequest field
-    case USB_LED_ON:
-		PORTB |= 1; // turn LED on
-		return 0;
-	case USB_LED_OFF: 
-		PORTB &= ~1; // turn LED off
-		return 0;
-    case USB_DATA_OUT: // send data to PC
-        usbMsgPtr = replyBuf;
-        return sizeof(replyBuf);
-	case USB_DATA_WRITE: // modify reply buffer
-		replyBuf[7] = rq->wValue.bytes[0];
-		replyBuf[8] = rq->wValue.bytes[1];
-		replyBuf[9] = rq->wIndex.bytes[0];
-		replyBuf[10] = rq->wIndex.bytes[1];
-		return 0;
-    case USB_DATA_IN: // receive data from PC
-		dataLength  = (uchar)rq->wLength.word;
-        dataReceived = 0;
-		
-		if(dataLength  > sizeof(replyBuf)) // limit to buffer size
-			dataLength  = sizeof(replyBuf);
-			
-		return USB_NO_MSG; // usbFunctionWrite will be called now
+usbMsgLen_t usbFunctionSetup(uchar data[8]) {
+    usbRequest_t *rq = (void *)data;
+
+    switch(rq->bRequest){
+        case VENDOR_RQ_READ_BUFFER:
+            currentPosition = 0;                // initialize position index
+            bytesRemaining = rq->wLength.word;  // store the amount of data requested
+            return USB_NO_MSG;                          // tell driver to use usbFunctionRead()
     }
 
-    return 0; // should not get here
+    return 0; // by default don't return any data
 }
 
 // This gets called when data is sent from PC to the device
@@ -66,10 +77,44 @@ USB_PUBLIC uchar usbFunctionWrite(uchar *data, uchar len) {
     return (dataReceived == dataLength); // 1 if we received it all, 0 if not
 }
 
+uchar getData(uchar position){
+    return 0;
+}
+
+uchar usbFunctionRead(uchar *data, uchar len)
+{
+    uchar i;
+    if(len > bytesRemaining)                // len is max chunk size
+        len = bytesRemaining;               // send an incomplete chunk
+    bytesRemaining -= len;
+    for(i = 0; i < len; i++)
+        data[i] = getData(currentPosition); // copy the data to the buffer
+    return len;                             // return real chunk size
+}
+
+void pinInit() {
+    DDRB |= _BV(PB0); // PB0 as input
+    PORTB |= _BV(PB0);
+    DDRB &= ~_BV(PB1); //PB1 as output
+    PORTB &= ~_BV(PB1);
+}
+
+uchar getInterruptData(uchar *pos) {
+    wdt_reset();
+    if (PINB ^ _BV(PB0)) {
+        PORTB &= ~_BV(PB1);
+        *pos = 25;
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 int main() {
 	uchar i;
 
-	DDRB = 1; // PB0 as output
+	pinInit();
 	
     wdt_enable(WDTO_1S); // enable 1s watchdog timer
 
@@ -83,10 +128,22 @@ int main() {
     usbDeviceConnect();
 	
     sei(); // Enable interrupts after re-enumeration
-	
+
     while(1) {
         wdt_reset(); // keep the watchdog happy
         usbPoll();
+
+        if(usbInterruptIsReady()) {
+            uchar *p;
+            p = 0;
+            uchar len = getInterruptData(p); // obtain chunk of max 8 bytes
+            PORTB |= _BV(PB1);
+            if(len > 0) {                         // only send if we have data
+                usbSetInterrupt(p, len);
+            }
+        }
+
+
     }
 	
     return 0;
--- usbdrv/usbconfig.h
+++ usbdrv/usbconfig.h
@@ -74,7 +74,7 @@ section at the end of this file).
 
 /* --------------------------- Functional Range ---------------------------- */
 
-#define USB_CFG_HAVE_INTRIN_ENDPOINT    0
+#define USB_CFG_HAVE_INTRIN_ENDPOINT    1
 /* Define this to 1 if you want to compile a version with two endpoints: The
  * default control endpoint 0 and an interrupt-in endpoint (any other endpoint
  * number).
@@ -109,7 +109,7 @@ section at the end of this file).
  * (e.g. HID), but never want to send any data. This option saves a couple
  * of bytes in flash memory and the transmit buffers in RAM.
  */
-#define USB_CFG_INTR_POLL_INTERVAL      10
+#define USB_CFG_INTR_POLL_INTERVAL      100
 /* If you compile a version with endpoint 1 (interrupt-in), this is the poll
  * interval. The value is in milliseconds and must not be less than 10 ms for
  * low speed devices.
@@ -123,12 +123,12 @@ section at the end of this file).
  * The value is in milliamperes. [It will be divided by two since USB
  * communicates power requirements in units of 2 mA.]
  */
-#define USB_CFG_IMPLEMENT_FN_WRITE      1
+#define USB_CFG_IMPLEMENT_FN_WRITE      0
 /* Set this to 1 if you want usbFunctionWrite() to be called for control-out
  * transfers. Set it to 0 if you don't need it and want to save a couple of
  * bytes.
  */
-#define USB_CFG_IMPLEMENT_FN_READ       0
+#define USB_CFG_IMPLEMENT_FN_READ       1
 /* Set this to 1 if you need to send control replies which are generated
  * "on the fly" when usbFunctionRead() is called. If you only want to send
  * data from a static buffer, set it to 0 and return the data from
@@ -266,12 +266,12 @@ section at the end of this file).
  * to fine tune control over USB descriptors such as the string descriptor
  * for the serial number.
  */
-#define USB_CFG_DEVICE_CLASS        0xff    /* set to 0 if deferred to interface */
+#define USB_CFG_DEVICE_CLASS        3    /* set to 0 if deferred to interface */
 #define USB_CFG_DEVICE_SUBCLASS     0
 /* See USB specification if you want to conform to an existing device class.
  * Class 0xff is "vendor specific".
  */
-#define USB_CFG_INTERFACE_CLASS     0   /* define class here if not at device level */
+#define USB_CFG_INTERFACE_CLASS     3   /* define class here if not at device level */
 #define USB_CFG_INTERFACE_SUBCLASS  0
 #define USB_CFG_INTERFACE_PROTOCOL  0
 /* See USB specification if you want to conform to an existing device class or
@@ -279,7 +279,7 @@ section at the end of this file).
  * HID class is 3, no subclass and protocol required (but may be useful!)
  * CDC class is 2, use subclass 2 and protocol 1 for ACM
  */
-/* #define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    42 */
+#define USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH    52
 /* Define this to the length of the HID report descriptor, if you implement
  * an HID device. Otherwise don't define it or define it to 0.
  * If you use this define, you must add a PROGMEM character array named
